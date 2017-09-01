e4d32318d0138324490c3718a9bf942de300bbcf
PieterDr <pieter.drijkoningen@vrt.be>
2017-09-01 18:06:39 +0200

    add empty implementation of sudokusolver

---
+++ src/main/java/com/axxes/hackathon/solution/SudokuSolverImpl.java
@@ -0,0 +1,11 @@
+package com.axxes.hackathon.olution;
+
+import com.axxes.hackathon.axxercise.exercise.SudokuSolver;
+
+public class SudokuSolverImpl implements SudokuSolver {
+
+    @Override
+    public Integer[][] solveSudoku(Integer[][] integers) {
+        return new Integer[0][];
+    }
+}
