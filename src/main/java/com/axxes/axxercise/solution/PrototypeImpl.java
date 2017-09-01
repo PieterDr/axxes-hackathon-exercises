edc813af5b3cc043ca4ed87e6bcade284695a1d3
PieterDr <pieter.drijkoningen@vrt.be>
2017-09-01 14:58:58 +0200

    add empty implementation of exercise

---
+++ src/main/java/com/axxes/axxercise/solution/PrototypeImpl.java
@@ -0,0 +1,11 @@
+package com.axxes.axxercise.solution;
+
+import com.axxes.hackathon.axxercise.exercise.Prototype;
+
+public class PrototypeImpl implements Prototype {
+
+    @Override
+    public int calculateSum(int i, int i1) {
+        return 0;
+    }
+}
